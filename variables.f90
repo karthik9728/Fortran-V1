afb6c48d0cc9cb7a41acb310552ce56e046a34d9
Karthik <codewithkarthik@outlook.com>
2025-03-21 23:29:42 +0530

    Variables

--- variables.f90
+++ variables.f90
@@ -1,22 +1,34 @@
-program variables
+program variables    
     implicit none
 
-    ! Initialized
+    ! Declaring variable
     integer ::age
     real ::PI
-    character ::name
+    character ::initial
+    character(len=15):: name
+    character(:), allocatable :: message
     logical :: isOkay
 
+    ! Initialize variable
+    integer :: amount = 10
+
     !Assigned Value
-    age = 10
+    age = 20
     PI = 3.14
-    name = 'K'
+    initial = 'P'
+    name ='Karthik bnreiogbiorwegnioewngioewn'
+    message = "Let's Learn World's 1st Programming Language"
     isOkay = .true.
+
+
 
     print *,'Age is',age
     print *,'PI value is',PI
+    print *,'Initial: ',initial
     print *,'Name: ',name
+    print *,'Message: ',message
     print *,'Condition Value',isOkay 
+    print *,'Value of Amount:',amount
 
     ! Modern Fortran formatted output
     ! write(*, '(A, I0)') 'Age is ', age
