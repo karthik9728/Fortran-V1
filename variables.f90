9d172a56b223e6183026254646d6dc508039dc3e
Karthik <codewithkarthik@outlook.com>
2025-03-21 22:53:58 +0530

    variables

--- variables.f90
+++ variables.f90
@@ -2,14 +2,23 @@ program variables
     implicit none
 
     ! Initialized
-    integer ::age;
+    integer ::age
+    real ::PI
+    character ::name
+    logical :: isOkay
 
     !Assigned Value
-    age = 10;
+    age = 10
+    PI = 3.14
+    name = 'K'
+    isOkay = .true.
 
-    print *,'Age is ',age
+    print *,'Age is',age
+    print *,'PI value is',PI
+    print *,'Name: ',name
+    print *,'Condition Value',isOkay 
 
     ! Modern Fortran formatted output
-    write(*, '(A, I0)') 'Age is ', age
+    ! write(*, '(A, I0)') 'Age is ', age
 
 end program variables
